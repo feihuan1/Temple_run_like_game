0e40b14fe50d9c20e25d4880d85b72d96a93a437
feihuan1 <ericrealtor1997@gmail.com>
2024-07-16 21:15:10 -0700

    play around

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Vector3 offset;
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/PlayerMovements.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f; 
+    public float sidewayForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); 
+        
+        if(Input.GetKey(KeyCode.D)) {
+            rb.AddForce(sidewayForce*Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            rb.AddForce(-sidewayForce*Time.deltaTime, 0, 0);
+        }
+    }
+}
