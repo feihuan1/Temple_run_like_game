8c38034fbfe1f7fd884a261a7a15bdae16ff1c3f
feihuan1 <ericrealtor1997@gmail.com>
2024-07-20 22:24:49 -0700

    UI

---
+++ Assets/Scripts/EndTriger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EndTriger : MonoBehaviour
+{
+
+    public GameManager gameManager;
+
+ void OnTriggerEnter()
+ {
+    gameManager.completeLevel();
+ }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement; // changing or reload the scene
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
-    public float restartDelay = 1f;// 1 second
+    public float restartDelay = 1f;// 1 second 
+
+    public GameObject completeLevelUI;
+
+    public void completeLevel ()
+    {
+        completeLevelUI.SetActive(true); // this is the check on the top left enable the entire object
+    }
     public void EndGame()
     {   
         if(!gameHasEnded)
